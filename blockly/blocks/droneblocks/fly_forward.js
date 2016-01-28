becb34fefb5971b95369efa28fe5b5be62ff142a
Dennis Baldwin <dennisbaldwin@gmail.com>
2016-01-27 20:47:02 -0600

    Only allowing fly forward behavior for now. Users can yaw and fly forward to achieve flight in other direction

--- blockly/blocks/droneblocks/fly_forward.js
+++ blockly/blocks/droneblocks/fly_forward.js
@@ -1,8 +1,6 @@
 Blockly.Blocks['fly_forward'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("fly")
-    .appendField(new Blockly.FieldDropdown([["forward", "fly_forward"], ["backward", "fly_backward"], ["left", "fly_left"], ["right", "fly_right"]]), "direction");
+    this.appendDummyInput().appendField("fly forward");
     this.appendValueInput("distance").setCheck("Number");
     this.appendDummyInput().appendField("ft");
     this.setInputsInline(true);
