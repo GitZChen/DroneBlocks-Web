db5dd7aea063703c4f91835804a7dc1200cd9cce
Dennis Baldwin <dennisbaldwin@gmail.com>
2017-01-01 07:07:11 -0600

    DJI missions don't allow pitching the gimbal above 0

--- blockly/blocks/droneblocks/pitch_gimbal.js
+++ blockly/blocks/droneblocks/pitch_gimbal.js
@@ -2,7 +2,7 @@ Blockly.Blocks['pitch_gimbal_to'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("pitch gimbal to")
-        .appendField(new Blockly.FieldDropdown([["+30", "30"],["+15", "15"],["0", "0"], ["-15", "-15"], ["-30", "-30"], ["-45", "-45"], ["-60", "-60"], ["-75", "-75"], ["-90", "-90"]]), "angle")
+        .appendField(new Blockly.FieldDropdown([["0", "0"], ["-15", "-15"], ["-30", "-30"], ["-45", "-45"], ["-60", "-60"], ["-75", "-75"], ["-90", "-90"]]), "angle")
         .appendField("degrees");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
