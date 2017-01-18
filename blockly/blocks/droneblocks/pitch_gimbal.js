4b40a0279291645409f1b72eb466933f18651629
Dennis Baldwin <dennisbaldwin@gmail.com>
2017-01-17 22:30:56 -0600

    Changing gimbal block to accept values between 0 and -90

--- blockly/blocks/droneblocks/pitch_gimbal.js
+++ blockly/blocks/droneblocks/pitch_gimbal.js
@@ -1,8 +1,8 @@
-Blockly.Blocks['pitch_gimbal_to'] = {
+Blockly.Blocks['pitch_gimbal'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("pitch gimbal to")
-        .appendField(new Blockly.FieldDropdown([["0", "0"], ["-5", "-5"], ["-10", "-10"], ["-15", "-15"], ["-20", "-20"], ["-25", "-25"], ["-30", "-30"], ["-35", "-35"], ["-40", "-40"], ["-45", "-45"], ["-50", "-50"], ["-55", "-55"], ["-60", "-60"], ["-65", "-65"], ["-70", "-70"], ["-75", "-75"], ["-80", "-80"], ["-85", "-85"], ["-90", "-90"]]), "angle")
+        .appendField(new Blockly.FieldNumber('0', -90, 30, 1), 'angle')
         .appendField("degrees");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
