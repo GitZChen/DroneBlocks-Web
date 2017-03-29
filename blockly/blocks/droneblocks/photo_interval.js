8c77cefd086f6e417d6f5a61ac60416b9b67d71a
Dennis Baldwin <dennisbaldwin@gmail.com>
2017-03-29 16:08:24 -0500

    Max interval with DJI SDK is 30 seconds

--- blockly/blocks/droneblocks/photo_interval.js
+++ blockly/blocks/droneblocks/photo_interval.js
@@ -2,9 +2,9 @@ Blockly.Blocks['photo_interval'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("take")
-        .appendField(new Blockly.FieldNumber(2, 2, 100, 1), 'photo_count')
+        .appendField(new Blockly.FieldNumber(5, 1, 100, 1), 'photo_count')
         .appendField("photos with")
-        .appendField(new Blockly.FieldNumber(5, 1, 60, 1), 'interval')
+        .appendField(new Blockly.FieldNumber(5, 1, 30, 1), 'interval')
         .appendField("sec interval");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
