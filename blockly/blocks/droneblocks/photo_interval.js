5ae7d16967b9279d245ecae38f44a08e9deb5e8d
Dennis Baldwin <dennisbaldwin@gmail.com>
2017-04-02 08:37:27 -0500

    Setting minimum interval to 2 since 1s doesn't work

--- blockly/blocks/droneblocks/photo_interval.js
+++ blockly/blocks/droneblocks/photo_interval.js
@@ -4,7 +4,7 @@ Blockly.Blocks['photo_interval'] = {
         .appendField("take")
         .appendField(new Blockly.FieldNumber(5, 1, 100, 1), 'photo_count')
         .appendField("photos with")
-        .appendField(new Blockly.FieldNumber(5, 1, 30, 1), 'interval')
+        .appendField(new Blockly.FieldNumber(5, 2, 30, 1), 'interval')
         .appendField("sec interval");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
