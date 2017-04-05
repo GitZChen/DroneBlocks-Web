c7f3cd0404de248bbf66a824e6dc74ab064904d2
Dennis Baldwin <dennisbaldwin@gmail.com>
2017-04-05 09:37:24 -0500

    Updating orbit block

--- blockly/blocks/droneblocks/orbit.js
+++ blockly/blocks/droneblocks/orbit.js
@@ -1,26 +1,14 @@
 Blockly.Blocks['orbit'] = {
   init: function() {
+    this.appendValueInput("radius")
+        .setCheck("Number")
+        .appendField("Orbit with radius");
     this.appendDummyInput()
-    .appendField("Orbit")
-    this.appendDummyInput()
-        .appendField("radius")
-        .appendField(new Blockly.FieldDropdown([["25", "25"], ["30", "30"], ["35", "35"], ["40", "40"]]), "radius")
-        .appendField("ft");
-    this.appendDummyInput()
-        .appendField("altitude")
-        .appendField(new Blockly.FieldDropdown([["10", "10"], ["15", "15"], ["20", "20"], ["25", "25"]]), "altitude")
-        .appendField("ft");
-    this.appendDummyInput()
-        .appendField("entry direction")
-        .appendField(new Blockly.FieldDropdown([["north", "north"], ["south", "south"], ["west", "west"], ["east", "east"]]), "direction");
-    this.appendDummyInput()
-        .appendField("heading")
-        .appendField(new Blockly.FieldDropdown([["forward", "forward"], ["backward", "backward"], ["facing center", "center"], ["facing outward", "outward"], ["remote control", "remote"]]), "heading");
-    this.appendDummyInput()
-        .appendField("rotation")
-        .appendField(new Blockly.FieldDropdown([["clockwise", "cw"], ["counter clockwise", "ccw"]]), "rotation");
+        .appendField("ft at ")
+        .appendField(new Blockly.FieldNumber('20', 5, 100, 1), 'velocity')
+        .appendField("°/sec");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
-    this.setColour(330);
+    this.setColour('#2A9D8F');
   }
 };
